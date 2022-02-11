e67b210f715d27b73f7d999a283d49e8864af5ca
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2022-02-11 05:25:19 -0800

    using css modules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import "./styles/App.css";
+import { Route, Routes } from "react-router-dom";
+
+import classes from "./styles/App.module.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className={classes.App}></div>;
 }
 
 export default App;
