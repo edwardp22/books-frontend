6c0585c45e3d8c60c4710d8e45dc67c914cc31e4
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2022-02-11 08:10:09 -0800

    use of async / await

--- src/state/actionCreators/books.js
+++ src/state/actionCreators/books.js
@@ -1,128 +1,110 @@
 import { ActionTypes } from "../actionTypes";
 import { toast } from "react-toastify";
 
-export const booksGet = () => (dispatch) => {
+export const booksGet = () => async (dispatch) => {
   dispatch(booksLoading());
 
-  return fetch(`${process.env.REACT_APP_API_URL}/books`, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then(
-      (response) => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error(
-            "Error " + response.status + ": " + response.statusText
-          );
-          throw error;
-        }
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/books`, {
+      headers: {
+        "Content-Type": "application/json",
       },
-      (error) => {
-        var errmess = new Error(error.message);
-        throw errmess;
-      }
-    )
-    .then((res) => res.json())
-    .then((res) => dispatch(booksAdd(res.records)))
-    .catch((error) => {
-      toast.error(error.message);
-      dispatch(booksFailed(error.message));
     });
+
+    if (!response.ok) {
+      var error = new Error(
+        "Error " + response.status + ": " + response.statusText
+      );
+      throw error;
+    }
+
+    const records = await response.json();
+
+    dispatch(booksAdd(records));
+  } catch (error) {
+    toast.error(error.message);
+    dispatch(booksFailed(error.message));
+  }
 };
 
-export const booksPost = (book) => (dispatch) => {
-  return fetch(`${process.env.REACT_APP_API_URL}/books`, {
-    method: "POST",
-    body: JSON.stringify(book),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then(
-      (response) => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error(
-            "Error " + response.status + ": " + response.statusText
-          );
-          throw error;
-        }
+export const booksPost = (book) => async (dispatch) => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/books`, {
+      method: "POST",
+      body: JSON.stringify(book),
+      headers: {
+        "Content-Type": "application/json",
       },
-      (error) => {
-        var errmess = new Error(error.message);
-        throw errmess;
-      }
-    )
-    .then((response) => response.json())
-    .then((response) => dispatch(booksAdd(response)))
-    .catch((error) => {
-      console.log("Post books", error.message);
-      toast.error(error.message);
     });
+
+    if (!response.ok) {
+      var error = new Error(
+        "Error " + response.status + ": " + response.statusText
+      );
+      throw error;
+    }
+
+    const records = await response.json();
+
+    dispatch(booksAdd(records));
+  } catch (error) {
+    console.log("Post books", error.message);
+    toast.error(error.message);
+  }
 };
 
-export const booksPut = (id, book) => (dispatch) => {
-  return fetch(`${process.env.REACT_APP_API_URL}/books/${id}`, {
-    method: "PUT",
-    body: JSON.stringify(book),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then(
-      (response) => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error(
-            "Error " + response.status + ": " + response.statusText
-          );
-          throw error;
-        }
-      },
-      (error) => {
-        var errmess = new Error(error.message);
-        throw errmess;
+export const booksPut = (id, book) => async (dispatch) => {
+  try {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/books/${id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(book),
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-    )
-    .then((response) => response.json())
-    .then((response) => dispatch(booksAdd(response)))
-    .catch((error) => {
-      console.log("Put books", error.message);
-      toast.error(error.message);
-    });
+    );
+
+    if (!response.ok) {
+      var error = new Error(
+        "Error " + response.status + ": " + response.statusText
+      );
+      throw error;
+    }
+
+    const records = await response.json();
+
+    dispatch(booksAdd(records));
+  } catch (error) {
+    console.log("Put books", error.message);
+    toast.error(error.message);
+  }
 };
 
-export const booksDelete = (id) => (dispatch) => {
-  return fetch(`${process.env.REACT_APP_API_URL}/books/${id}`, {
-    method: "DELETE",
-  })
-    .then(
-      (response) => {
-        if (response.ok) {
-          return response;
-        } else {
-          var error = new Error(
-            "Error " + response.status + ": " + response.statusText
-          );
-          throw error;
-        }
-      },
-      (error) => {
-        var errmess = new Error(error.message);
-        throw errmess;
+export const booksDelete = (id) => async (dispatch) => {
+  try {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_URL}/books/${id}`,
+      {
+        method: "DELETE",
       }
-    )
-    .then((response) => response.json())
-    .then((response) => dispatch(booksAdd(response)))
-    .catch((error) => {
-      console.log("Delete books", error.message);
-      toast.error(error.message);
-    });
+    );
+
+    if (!response.ok) {
+      var error = new Error(
+        "Error " + response.status + ": " + response.statusText
+      );
+      throw error;
+    }
+
+    const records = await response.json();
+
+    dispatch(booksAdd(records));
+  } catch (error) {
+    console.log("Delete books", error.message);
+    toast.error(error.message);
+  }
 };
 
 const booksLoading = () => ({
