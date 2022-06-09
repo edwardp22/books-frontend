bd176543e3c10a230a9e986288585a7db9a04b49
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2022-06-09 04:59:10 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -7,18 +7,6 @@ import List from "./pages/books/List";
 
 import classes from "./styles/global.module.css";
 
-//Server to heroku
-const express = require("express");
-const path = require("path");
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.static(path.join(__dirname, "build")));
-
-app.listen(port, () => console.log(`App is live on port ${port}!`));
-//Server to heroku
-
 function App() {
   return (
     <div className={classes.App}>
